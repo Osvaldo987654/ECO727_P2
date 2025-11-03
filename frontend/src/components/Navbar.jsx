2473e0179617b90e06ae5926f23ef20079353388
Osvaldo Cisneros <cifmosvaldo@gmail.com>
2025-11-03 00:06:52 -0600

    Modificacion

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,13 +2,13 @@ export default function Navbar() {
   return (
     <header>
       <nav className="navbar">
-        <a href="#inicio">Inicio</a>
-        <a href="#nosotros">Sobre Nosotros</a>
-        <a href="#mision-vision">Misión y Visión</a>
-        <a href="#servicios">Servicios</a>
-        <a href="#galeria">Galería</a>
-        <a href="#testimonios">Testimonios</a>
-        <a href="#contacto">Contacto</a>
+        <a href="#inicio">Inicio | </a>
+        <a href="#nosotros">Sobre Nosotros | </a>
+        <a href="#mision-vision">Misión y Visión | </a>
+        <a href="#servicios">Servicios | </a>
+        <a href="#galeria">Galería | </a>
+        <a href="#testimonios">Testimonios | </a>
+        <a href="#contacto">Contacto | </a>
       </nav>
     </header>
   );
