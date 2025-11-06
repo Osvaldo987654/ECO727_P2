cafbdae7589d1e15b4e07abde0ee11f04383fd3c
Osvaldo Cisneros <cifmosvaldo@gmail.com>
2025-11-06 17:00:12 -0600

    AXIO.JS con la nueva ruta de la api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://eco727-p2.onrender.com/api", // Dominio generado en render
+  baseURL: "https://eco727-p2.onrender.com/api/canciones", // Dominio generado en render
 });
