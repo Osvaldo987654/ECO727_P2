ecd1051980b33d3ef4499d48def66f131c2ff63f
Osvaldo Cisneros <cifmosvaldo@gmail.com>
2025-11-06 18:37:32 -0600

    Edicion en el menú

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,12 +3,13 @@ export default function Navbar() {
     <header>
       <nav className="navbar">
         <a href="#inicio">| Inicio | </a>
-        <a href="#nosotros">| Sobre Nosotros | </a>
-        <a href="#mision-vision">| Misión y Visión | </a>
-        <a href="#servicios">| Servicios | </a>
-        <a href="#galeria">| Galería | </a>
-        <a href="#testimonios">| Testimonios | </a>
+        <a href="#nosotros"> Sobre Nosotros | </a>
+        <a href="#mision-vision"> Misión y Visión | </a>
+        <a href="#servicios"> Servicios | </a>
+        <a href="#galeria"> Galería | </a>
+        <a href="#testimonios"> Testimonios | </a>
         <a href="#contacto">Contacto | </a>
+        <a href="#canciones">Canciones | </a>
       </nav>
     </header>
   );
